73660c5b2acadd411c3c014c6d50d812dbb0d1b7
suzana <antolin.suzana@gmail.com>
2023-01-20 18:57:06 +0100

    Jumping fixed. PlayerController extended

---
+++ Assets/Scripts/Characters/BasePlayerController.cs
@@ -0,0 +1,236 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Data;
+using Global;
+using MyGame;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.InputSystem;
+using Weapons;
+
+namespace Characters
+{
+    public abstract class BasePlayerController : MonoBehaviour, IPlayer
+    {
+        [SerializeField]
+        protected GameObject gameManager;
+        [SerializeField]
+        protected Camera camera;
+        [SerializeField]
+        protected HealthBar healthBar;
+        [SerializeField]
+        protected GameObject charactersContainer;
+        [SerializeField]
+        protected CharacterController characterController;
+        [SerializeField]
+        protected Animator animator;
+        [SerializeField]
+        protected float sensitivity;
+        [SerializeField]
+        protected float speed;
+        [SerializeField]
+        protected float jumpHeight;
+        [SerializeField]
+        protected Vector3 velocity = Vector3.zero;
+        [SerializeField]
+        protected Transform groundDetector;
+        [SerializeField]
+        protected LayerMask groundLayer;
+        [SerializeField]
+        protected AudioSource sfxAudioSource;
+        [SerializeField]
+        protected AudioClip walkAudioClip;
+        [SerializeField]
+        protected AudioClip jumpAudioClip;
+        [SerializeField]
+        protected AudioClip hurtAudioClip;
+
+        protected float _currentHealth;
+        protected PlayerInputActions _playerInput;
+        protected InputAction _move;
+        protected Keyboard _keyboard;
+        protected InputAction _look;
+        protected InputAction _jump;
+        protected InputAction _run;
+        protected Mouse _mouse;
+        protected float _rotation;
+        protected float _gravity = 1.8f;
+        protected float _cameraJumpHeight;
+
+        protected static readonly int Run = Animator.StringToHash("run");
+        protected static readonly int JumpTriggerAnim = Animator.StringToHash("jump");
+        protected static readonly int SpeedFloatAnim = Animator.StringToHash("speed");
+        protected static readonly int DieTriggerAnim = Animator.StringToHash("die");
+
+        #region Unity methods
+
+        private void Awake()
+        {
+            _playerInput = new PlayerInputActions();
+            _mouse = InputSystem.GetDevice<Mouse>();
+            _keyboard = InputSystem.GetDevice<Keyboard>();
+            _cameraJumpHeight = jumpHeight * 5;
+        }
+
+        protected abstract void Start();
+
+        protected abstract void Update();
+
+
+        protected void OnEnable()
+        {
+            _move = _playerInput.Player.Move;
+            _move.Enable();
+
+            _jump = _playerInput.Player.Jump;
+            _jump.Enable();
+            _jump.performed += JumpInput;
+
+
+            _look = _playerInput.Player.Look;
+            _look.Enable();
+        }
+
+        protected void OnDisable()
+        {
+            _look.Disable();
+            _move.Disable();
+            _jump.Disable();
+        }
+
+        protected void OnDrawGizmos()
+        {
+            Gizmos.DrawSphere(groundDetector.position, 0.3f);
+        }
+
+        #endregion
+
+        protected void RunIfAltKeyIsPressed()
+        {
+            if (_keyboard.altKey.wasPressedThisFrame)
+            {
+                animator.SetBool(Run, true);
+                speed += 5;
+                PlayRunSoundFx();
+            }
+            else if (_keyboard.altKey.isPressed)
+            {
+                PlayRunSoundFx();
+            }
+
+            else if (_keyboard.altKey.wasReleasedThisFrame)
+            {
+                speed -= 5;
+                animator.SetBool(Run, false);
+            }
+        }
+
+        protected void PlayWalkSoundFx()
+        {
+            if (sfxAudioSource.isPlaying)
+                return;
+
+            sfxAudioSource.pitch = 0.5f;
+            sfxAudioSource.PlayOneShot(walkAudioClip);
+        }
+        protected void PlayJumpSoundFx()
+        {
+            if (sfxAudioSource.isPlaying)
+                sfxAudioSource.Stop();
+
+            sfxAudioSource.pitch = 0.5f;
+            sfxAudioSource.PlayOneShot(jumpAudioClip);
+        }
+
+        protected void PlayRunSoundFx()
+        {
+            if (sfxAudioSource.isPlaying)
+                return;
+            sfxAudioSource.pitch = 1f;
+            sfxAudioSource.PlayOneShot(walkAudioClip);
+        }
+
+        public abstract bool IsVictory();
+        public abstract void GameOver();
+        protected abstract void HandleRotation();
+
+        protected void HandleMovement()
+        {
+            float moveX = _move.ReadValue<Vector2>().x;
+            float moveZ = _move.ReadValue<Vector2>().y;
+
+            Transform t = characterController.transform;
+
+            Vector3 moveVector = t.forward * moveZ;
+            moveVector += t.right * moveX;
+            moveVector *= speed * Time.deltaTime;
+
+            /*gravity*/
+            velocity.y -= _gravity * Time.deltaTime;
+            if (IsGrounded() && velocity.y < 0)
+            {
+                velocity.y = 0;
+            }
+
+            AnimateMovement(moveVector);
+            characterController.Move(moveVector + velocity);
+        }
+
+
+        protected void AnimateMovement(Vector3 moveVector)
+        {
+            if (IsPlayerRunning()) return;
+            if (moveVector.x > 0 || moveVector.z > 0)
+            {
+                animator.SetFloat(SpeedFloatAnim, speed);
+                PlayWalkSoundFx();
+            }
+            else
+            {
+                animator.SetFloat(SpeedFloatAnim, 0);
+                StopCurrentSoundFx();
+            }
+        }
+
+        protected void StopCurrentSoundFx()
+        {
+            sfxAudioSource.Stop();
+        }
+
+        private bool IsPlayerRunning() => _keyboard.altKey.isPressed;
+
+        protected bool IsGrounded()
+        {
+            return Physics.CheckSphere(groundDetector.position, 0.3f, groundLayer);
+        }
+
+        protected IEnumerator JumpAction()
+        {
+            animator.SetTrigger(JumpTriggerAnim);
+            Debug.Log($"[JumpAction 1] Velocity.y {velocity.y} ");
+
+            yield return new WaitForSeconds(0.5f);
+
+            velocity.y = jumpHeight;
+            PlayJumpSoundFx();
+            Debug.Log($"[JumpAction 2] Velocity.y {velocity.y} ");
+
+        }
+        protected void JumpInput(InputAction.CallbackContext ctx)
+        {
+            if (IsGrounded())
+            {
+                StartCoroutine(JumpAction());
+            }
+        }
+
+        #region IPlayer methods
+
+        public abstract IEnumerator TakeDamage(float damageAmount);
+        public abstract void Heal(int healAmount);
+        public abstract void GrabVaccineBag();
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Characters/FpsPlayerController.cs
@@ -0,0 +1,250 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Data;
+using Global;
+using MyGame;
+using UnityEditor.Recorder.Input;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.InputSystem;
+using Weapons;
+
+namespace Characters
+{
+    public class FpsPlayerController : BasePlayerController
+    {
+        [SerializeField] private Transform shootFromPoint;
+        [SerializeField] private GameObject scratchView;
+
+        private InputAction _fire;
+        private InputAction _weaponChange;
+        private Weapon _selectedWeapon;
+        private int _currentWeaponIndex;
+        private List<Weapon> _weapons;
+        private IWeaponService _weaponService;
+        private static readonly int ShootTriggerAnim = Animator.StringToHash("shoot");
+
+        #region Unity methods
+
+        protected override void Start()
+        {
+            _currentHealth = PlayerPrefs.GetFloat(PlayerPrefNames.Health);
+            _weaponService = ServiceProvider.WeaponService();
+            _weapons = _weaponService.GetWeapons(shootFromPoint);
+            SelectWeapon();
+
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+
+        protected override void Update()
+        {
+            #if ENABLE_INPUT_SYSTEM
+            HandleRotation();
+            HandleMovement();
+            RunIfAltKeyIsPressed();
+            #endif
+
+            #if ENABLE_LEGACY_INPUT_MANAGER
+            HandleRotation_OldInputSystem();
+            HandleMovement_OldInputSystem();
+            RunIfAltKeyIsPressed();
+            #endif
+
+            //velocity.y -= _gravity * Time.deltaTime;
+            //characterController.Move(velocity * Time.deltaTime);
+
+            if (IsVictory())
+            {
+                Debug.Log("VICTORY");
+                gameManager.GetComponent<IGameManager>().PlayLevelCompleted();
+            }
+        }
+
+        protected new void OnEnable()
+        {
+            base.OnEnable();
+            _fire = _playerInput.Player.Fire;
+            _fire.Enable();
+            _fire.performed += Shoot;
+
+            _weaponChange = _playerInput.Player.WeaponChange;
+            _weaponChange.Enable();
+            _weaponChange.performed += ChangeWeapon;
+        }
+
+        protected new void OnDisable()
+        {
+            base.OnDisable();
+            _fire.Disable();
+            _weaponChange.Disable();
+        }
+
+        #endregion
+
+
+        public override bool IsVictory()
+        {
+            /**Victory is when there are no more enemies in character container*/
+            var characters = charactersContainer.GetComponentsInChildren<Transform>();
+            return characters.All(item => !item.gameObject.CompareTag("Enemy"));
+        }
+
+        public override void GameOver()
+        {
+            PlayerPrefs.SetFloat(PlayerPrefNames.Health, 0);
+            _currentHealth = 0;
+
+            //stop enemies
+            Transform[] enemies = charactersContainer.GetComponentsInChildren<Transform>();
+            foreach (Transform enemy in enemies)
+            {
+                if (enemy.TryGetComponent(out IEnemy enemyCharacter))
+                {
+                    enemyCharacter.Stop();
+                }
+            }
+
+            sfxAudioSource.Stop();
+            animator.SetTrigger(DieTriggerAnim);
+            gameManager.GetComponent<IGameManager>().PlayGameOverTimeline();
+        }
+
+        protected override void HandleRotation()
+        {
+            Vector2 delta = _mouse.delta.ReadValue();
+            float x = delta.x * sensitivity * Time.deltaTime;
+            float y = delta.y * sensitivity * Time.deltaTime;
+
+            characterController.transform.Rotate(Vector3.up * x);
+
+            _rotation -= y;
+            _rotation = Mathf.Clamp(_rotation, -70f, 60f);
+
+            camera.transform.localRotation = Quaternion.Euler(_rotation, 0f, 0f);
+            shootFromPoint.localRotation = Quaternion.Euler(_rotation,   0f, 0f);
+        }
+
+        #region IPlayer methods
+
+        public override IEnumerator TakeDamage(float damageAmount)
+        {
+            _currentHealth -= damageAmount;
+            if (_currentHealth <= 0)
+            {
+                GameOver();
+            }
+            else
+            {
+                PlayerPrefs.SetFloat(PlayerPrefNames.Health, _currentHealth);
+                healthBar.SetHealth(_currentHealth);
+                scratchView.SetActive(true);
+                var image = scratchView.GetComponentInChildren<Canvas>();
+
+
+                yield return new WaitForSecondsRealtime(0.4f);
+                image.enabled = false;
+
+                yield return new WaitForSecondsRealtime(0.2f);
+                image.enabled = true;
+
+                yield return new WaitForSecondsRealtime(0.2f);
+                scratchView.SetActive(false);
+            }
+        }
+
+        public override void Heal(int healAmount)
+        {
+            _currentHealth += healAmount;
+            if (_currentHealth > GameData.MaxPlayerHealth)
+            {
+                _currentHealth = GameData.MaxPlayerHealth;
+            }
+
+            PlayerPrefs.SetFloat(PlayerPrefNames.Health, _currentHealth);
+            healthBar.SetHealth(_currentHealth);
+        }
+
+        public override void GrabVaccineBag()
+        {
+            Debug.Log("Not implemented here.");
+        }
+
+        #endregion
+
+
+        private bool CanShoot()
+        {
+            //TODO
+            return true;
+        }
+
+        private IEnumerator ShootAction()
+        {
+            animator.SetTrigger(ShootTriggerAnim);
+
+            yield return new WaitForSeconds(1f);
+
+            _selectedWeapon.Shoot();
+        }
+
+        private void Shoot(InputAction.CallbackContext ctx)
+        {
+            if (CanShoot())
+            {
+                StartCoroutine(ShootAction());
+            }
+        }
+
+        private void ChangeWeapon(InputAction.CallbackContext ctx)
+        {
+            _currentWeaponIndex = (_currentWeaponIndex + 1) % _weapons.Count;
+            SelectWeapon();
+        }
+
+        protected void SelectWeapon()
+        {
+            _selectedWeapon = _weapons[_currentWeaponIndex];
+            UIManager.shared.SetWeaponName(_selectedWeapon.Name);
+        }
+
+        #region Old input system
+
+        private void HandleRotation_OldInputSystem()
+        {
+            float x = Input.GetAxis(InputNames.MouseX) * sensitivity * Time.deltaTime;
+            float y = Input.GetAxis(InputNames.MouseY) * sensitivity * Time.deltaTime;
+
+            characterController.transform.Rotate(Vector3.up * x);
+
+            _rotation -= y;
+            _rotation = Mathf.Clamp(_rotation, -70f, 60f);
+
+            camera.transform.localRotation = Quaternion.Euler(_rotation, 0f, 0f);
+            shootFromPoint.localRotation = Quaternion.Euler(_rotation,   0f, 0f);
+        }
+
+        private void HandleMovement_OldInputSystem()
+        {
+            float moveX = Input.GetAxis(InputNames.Horizontal);
+            float moveZ = Input.GetAxis(InputNames.Vertical);
+
+            var transform1 = characterController.transform;
+            Vector3 moveVector = transform1.forward * moveZ;
+            moveVector += transform1.right * moveX;
+
+            moveVector *= speed * Time.deltaTime;
+            velocity.y += _gravity * Time.deltaTime * Time.deltaTime;
+
+            if (IsGrounded() && velocity.y < 0)
+            {
+                velocity.y = 0;
+            }
+
+            characterController.Move(moveVector + velocity);
+        }
+
+        #endregion
+    }
+}
