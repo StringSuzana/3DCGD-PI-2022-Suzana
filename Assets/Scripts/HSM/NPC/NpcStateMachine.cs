ecf2831204b577f061d622b97d667c976aefd22f
suzana <antolin.suzana@gmail.com>
2023-01-26 18:50:03 +0100

    doctor is moving

--- Assets/Scripts/HSM/NPC/NpcStateMachine.cs
+++ Assets/Scripts/HSM/NPC/NpcStateMachine.cs
@@ -99,7 +99,7 @@ public class NpcStateMachine : MonoBehaviour
         CurrentState.UpdateStates();
         CurrentState.CheckSwitchStates();
 
-        if (!agent.pathPending && agent.remainingDistance < 0.5) HasNextWayPoint = false;
+        if (agent.hasPath && agent.remainingDistance < 0.5f) HasNextWayPoint = false;
     }
 
     private void OnTriggerEnter(Collider other)
